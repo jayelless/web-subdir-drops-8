f2629ddce8f7b446f01d1c314f66ad4de69adf98
James Scott <james@pallene.lan>
2021-01-07 14:03:14 +1300

    Convert default branch to web subdir docroot

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -13,6 +13,9 @@ class ComposerAutoloaderInit9de9436f91c599173609d35a420479fb
         }
     }
 
+    /**
+     * @return \Composer\Autoload\ClassLoader
+     */
     public static function getLoader()
     {
         if (null !== self::$loader) {
